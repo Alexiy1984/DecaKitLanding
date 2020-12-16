c3f19ebfabea29cc6bf572a85c2fc2fc82f3dce1
Alex <alevaliev@yandex.ru>
2020-12-16 22:56:04 +0300

    added buttons-icons, and popups blocks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,24 @@
 import '../styles/index.scss';
+import $ from "jquery";
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+$(window).on('resize load', function() {
+  if ($(window).outerWidth() < 480) {
+    $('.popups-block .flex-inner').height('auto');
+  } else {
+    if ($(window).outerWidth() < 768) {
+      var masonry_height = $('.popups-block .flex-inner__item').eq(0).outerHeight() 
+      + $('.popups-block .flex-inner__item').eq(2).outerHeight()
+      + $('.popups-block .flex-inner__item').eq(4).outerHeight();
+      $('.popups-block .flex-inner').height(masonry_height + 70);
+    } else {
+      var masonry_height = $('.popups-block .flex-inner__item').eq(1).outerHeight() + $('.popups-block .flex-inner__item').eq(4).outerHeight();
+      $('.popups-block .flex-inner').height(masonry_height + 50);
+    }
+  }
+});
+
+// console.log($('.popups-block .flex-inner__item').eq(2).html());
