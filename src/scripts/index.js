c5946cc1a41904f847705d3b603d8150866a036d
Alex <alevaliev@yandex.ru>
2020-12-17 17:48:27 +0300

    added vertical cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,15 +8,27 @@ if (process.env.NODE_ENV === 'development') {
 $(window).on('resize load', function() {
   if ($(window).outerWidth() < 480) {
     $('.popups-block .flex-inner').height('auto');
+    $('.vertical-cards-block .flex-inner').height('auto');
   } else {
     if ($(window).outerWidth() < 768) {
-      var masonry_height = $('.popups-block .flex-inner__item').eq(0).outerHeight() 
+      var popupsblock_height = $('.popups-block .flex-inner__item').eq(0).outerHeight() 
       + $('.popups-block .flex-inner__item').eq(2).outerHeight()
       + $('.popups-block .flex-inner__item').eq(4).outerHeight();
-      $('.popups-block .flex-inner').height(masonry_height + 70);
+      $('.popups-block .flex-inner').height(popupsblock_height + 70);
+      var verticalblock_height = $('.vertical-cards-block .flex-inner__item').eq(0).outerHeight() 
+      + $('.vertical-cards-block .flex-inner__item').eq(2).outerHeight()
+      + $('.vertical-cards-block .flex-inner__item').eq(4).outerHeight()
+      + $('.vertical-cards-block .flex-inner__item').eq(6).outerHeight()
+      + $('.vertical-cards-block .flex-inner__item').eq(8).outerHeight();
+      $('.vertical-cards-block .flex-inner').height(verticalblock_height + 150);
     } else {
-      var masonry_height = $('.popups-block .flex-inner__item').eq(1).outerHeight() + $('.popups-block .flex-inner__item').eq(4).outerHeight();
-      $('.popups-block .flex-inner').height(masonry_height + 50);
+      var popupsblock_height = $('.popups-block .flex-inner__item').eq(1).outerHeight() 
+      + $('.popups-block .flex-inner__item').eq(4).outerHeight();
+      $('.popups-block .flex-inner').height(popupsblock_height + 50);
+      var verticalblock_height = $('.vertical-cards-block .flex-inner__item').eq(1).outerHeight() 
+      + $('.vertical-cards-block .flex-inner__item').eq(4).outerHeight()
+      + $('.vertical-cards-block .flex-inner__item').eq(7).outerHeight();
+      $('.vertical-cards-block .flex-inner').height(verticalblock_height + 70);
     }
   }
 });
